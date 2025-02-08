41bcaf899cfb92ab940602a0133b0c2f8b8ad483
Sijan14 <sisijan18@earlham.edu>
2025-02-08 12:14:37 -0500

    random intercept model practice

--- Week 4/random_intercept_model.R
+++ Week 4/random_intercept_model.R
@@ -1,3 +1,39 @@
-git remote add origin https://github.com/Sijan14/Multilevel_Modeling.git
-git branch -M main
-git push -u origin main
+library(tidyverse)
+library(haven)
+library(lme4)
+library(lmerTest)
+
+df <- read_sav("lq2002.sav")
+
+"""
+• HOSTILE: Individual-level Hostility Scale score is the outcome variable.
+• TSIG: Individual-level Task Significance score is a level 1 covariate.
+• COMPID: This is the level 2 grouping (level) variable, defining 49 groups.
+• GTSIG: Company-level aggregate Task Significance score is a level 2 covariate.
+"""
+attach(df)
+
+# Null Model
+NullModel <- lmer(HOSTILE ~ (1|COMPID), REML = F, data = df)
+summary(NullModel)
+
+# Random Intercept Model
+RIModel <- lmer(HOSTILE ~ TSIG + GTSIG + (1|COMPID), REML = F, data = df)
+summary(RIModel)
+
+# Random effects (A Likelihood Ratio Test with a reduced model {random effect removed})
+rand(RIModel)
+"The random effect (1 | COMPID) significantly improves the model fit."
+
+step(RIModel)
+"The step() command will drop terms to arrive at a final model. However, in this example there are no non-significant terms, so the final model is the starting model."
+
+# Likelihood Ratio Test
+anova(NullModel, RIModel)
+
+# or
+library(lmtest)
+lrtest(NullModel, RIModel)
+
+# scaled residuals
+quantile(residuals(RIModel, "pearson", scaled = TRUE))
